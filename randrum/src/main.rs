72c53443b2c413af8a0eec759a1b4e66bc50d948
Logan <logannbjork1@gmail.com>
2025-12-21 23:22:53 -0500

    walkdir

--- randrum/src/main.rs
+++ randrum/src/main.rs
@@ -4,7 +4,12 @@ mod args;
 use args::RanDrumArgs;
 use clap::{Args, Parser, Subcommand};
 */
+use::walkdir::WalkDir;
 
 fn main() {
     // let args = RanDrumArgs::parse();
+    for entry in WalkDir::new("input") {
+        println!("{}", entry.unwrap().path().display());
+    }
+
 }
